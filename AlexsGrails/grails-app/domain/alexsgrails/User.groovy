2e6710e3afa8ba28d44f1b9aee2225ddc452a705
alektao <tao_yb918@qq.com>
2014-08-17 18:33:01 +0800

    init
    init

---
+++ AlexsGrails/grails-app/domain/alexsgrails/User.groovy
@@ -0,0 +1,13 @@
+package alexsgrails
+
+class User {
+
+    private String screenName;
+    private String province;
+    private String city;
+    private String location;
+    private String description;
+
+    static constraints = {
+    }
+}
